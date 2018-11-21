726d60f93d9cc8fe468640d11cda07f91ec098f7
Daizy <kxania@gmail.com>
2018-11-21 11:01:00 +0300

    Update database configurations with heroku database details

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from app.controllers.db import DatabaseConnection
 
 app = Flask(__name__)
+db.setUp()
 
 app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
 
